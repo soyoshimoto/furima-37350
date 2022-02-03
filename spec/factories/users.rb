0570388c72e81e3c5d63914a298ce8d3c07ccc42
soyoshimoto <96470876+soyoshimoto@users.noreply.github.com>
2022-02-03 20:37:47 +0900

    テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    lastname      {'aaaa'}
+    firstname     {'bbbb'}
+    lastname_ruby {'cccc'}
+    firstname_ruby {'dddd'}
+    birthday {'eeee'}
+    email {'ffff'}
+    password {'gggg'}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,33 +6,33 @@ RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
     it "nicknameが必須" do
       @user.nickname = ''
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it "firstnameが必須" do
       @user.firstname = ''
-      user.valid?
-      expect(user.errors.full_messages).to include("Firstname can't be blank")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Firstname can't be blank")
     end
     it "lastnameが必須"do
       @user.lastname = ''
-      user.valid?
-      expect(user.errors.full_messages).to include("Lastname can't be blank")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Lastname can't be blank")
     end
     it "firstname_rubyが必須"do
       @user.firstname_ruby = ''
-      user.valid?
-      expect(user.errors.full_messages).to include("Firstname_ruby can't be blank")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Firstname ruby can't be blank")
     end
     it "lastname_rubyが必須"do
       @user.lastname_ruby = ''
-      user.valid?
-      expect(user.errors.full_messages).to include("Lastname_ruby can't be blank")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Lastname ruby can't be blank")
     end
     it "birthdayが必須"do
       @user.birthday = ''
-      user.valid?
-      expect(user.errors.full_messages).to include("Birthday can't be blank")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
 end
