5120ce085d4612a00ffad1e4b2b5cddc95656629
soyoshimoto <96470876+soyoshimoto@users.noreply.github.com>
2022-02-03 09:09:45 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :firstname_ruby, presence: true
+  validates :lastname_ruby, presence: true
+  validates :birthday, presence: true
+
 end
