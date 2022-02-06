7c26ca30c2dcc9755ce3d6088c86cbf2b595054c
soyoshimoto <96470876+soyoshimoto@users.noreply.github.com>
2022-02-06 16:20:13 +0900

    マイグレーションファイルの編集

--- db/migrate/20220202084149_devise_create_users.rb
+++ db/migrate/20220202084149_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :lastname_ruby,      null: false
       t.string :firstname_ruby,     null: false
       t.string :nickname,           null: false
-      t.string :birthday,           null: false
+      t.date :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
