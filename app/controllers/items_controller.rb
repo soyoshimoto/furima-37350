f142f901ad9b702f80d89cbdfdf497dcdddd259c
soyoshimoto <96470876+soyoshimoto@users.noreply.github.com>
2022-02-07 21:16:51 +0900

    画像投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,18 @@
 class ItemsController < ApplicationController
   def index
-    @items = Itmem.all
+    @items = Item.all
   end
 
   def new
-    @items = Items.new
+    @items = Item.new
+  end
+
+  def create
+    Item.create(item_paramas)
+  end
+
+  private
+  def item_paramas
+    params.require(:item).permit(:items_name, :image, :explanation).maerge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :items_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :price, presence: true
+  validates :shipping_charges_burden_id, presence: true
+  validates :prefecture_id, presence: true
+
 end
