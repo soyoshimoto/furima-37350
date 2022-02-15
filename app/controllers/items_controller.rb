434af078e0bc2651f98ae29faa5af1ee15ef9d56
soyoshimoto <96470876+soyoshimoto@users.noreply.github.com>
2022-02-15 17:31:44 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(item.id)
     else 
